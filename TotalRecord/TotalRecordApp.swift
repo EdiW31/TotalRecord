8892200c799a56a986f20b74083256e783b18d36
EdiW31 <eduardweiss998@gmail.com>
2025-06-19 12:16:25 +0300

    Initial Commit

---
+++ TotalRecord/TotalRecordApp.swift
@@ -0,0 +1,17 @@
+//
+//  TotalRecordApp.swift
+//  TotalRecord
+//
+//  Created by Eduard Weiss on 19.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct TotalRecordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TotalRecordTests/TotalRecordTests.swift
@@ -0,0 +1,17 @@
+//
+//  TotalRecordTests.swift
+//  TotalRecordTests
+//
+//  Created by Eduard Weiss on 19.06.2025.
+//
+
+import Testing
+@testable import TotalRecord
+
+struct TotalRecordTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
